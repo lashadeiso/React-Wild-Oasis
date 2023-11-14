e05d8990961aa94f0e7fef4617f9d794646598bd
Lasha <lashiko.deisadze@gmail.com>
2023-11-14 17:10:16 +0400

    finished img upload filed

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://vhldrvdprajesatqdbcd.supabase.co";
+export const supabaseUrl = "https://vhldrvdprajesatqdbcd.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZobGRydmRwcmFqZXNhdHFkYmNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk3NzY5NjQsImV4cCI6MjAxNTM1Mjk2NH0.IGa8yaot34gCzlNvKjqe0oMJr83SVaMe1H3Y9qrR_YI";
 const supabase = createClient(supabaseUrl, supabaseKey);
