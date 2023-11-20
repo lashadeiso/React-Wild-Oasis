972fa2511a3cf9b0bf70f3a440754165c43f0437
Lasha <lashiko.deisadze@gmail.com>
2023-11-20 13:25:27 +0400

    finished booking section

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -41,23 +41,23 @@ function Filter({ filterField, options }) {
 
   function handleClick(value) {
     searchParams.set(filterField, value);
+    if (searchParams.get("page")) searchParams.set("page", 1);
+
     setSearchParams(searchParams);
   }
 
   return (
     <StyledFilter>
-      {options.map((option) => {
-        return (
-          <FilterButton
-            onClick={() => handleClick(option.value)}
-            key={option.value}
-            active={option.value === currentFilter}
-            disabled={option.value === currentFilter}
-          >
-            {option.label}
-          </FilterButton>
-        );
-      })}
+      {options.map((option) => (
+        <FilterButton
+          key={option.value}
+          onClick={() => handleClick(option.value)}
+          active={option.value === currentFilter}
+          disabled={option.value === currentFilter}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
     </StyledFilter>
   );
 }
